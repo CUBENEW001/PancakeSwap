ded4d74944de01b3c415a32bb493a15424458c2f
Gianni9728 <gianni9728@gmail.com>
2022-03-18 17:37:33 +0100

    small fix

--- src/app/Core/warning-banner/warning-banner.component.ts
+++ src/app/Core/warning-banner/warning-banner.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./warning-banner.component.css']
 })
 export class WarningBannerComponent implements OnInit {
+  Luca = true;
 
+  nasconDino() {
+   this.Luca= false;
+  };
 
   constructor() { }
 
